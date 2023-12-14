0de1162c9e29a0f9edbf0951ba33c371f57ee1f3
RaghadKhatatba <RaghadZKhatatba@gmail.com>
2023-12-14 15:28:36 +0200

    Format document and applying the router between pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import Home from './Pages/Home'
+import Search from './Pages/Search'
+import Favorite from './Pages/Favorite'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Components/Layout/Layout'
+import NotFound from './Components/NotFound/NotFound'
+
+let routers = createBrowserRouter([
+  {
+    path: '',
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'search', element: <Search /> },
+      { path: 'favorite', element: <Favorite /> },
+      { path: '*', element: <NotFound /> },
+    ],
+  },
+]);
 
 export default function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <RouterProvider router={routers}></RouterProvider>
   )
 }
